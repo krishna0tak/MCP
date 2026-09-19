97c5d17026d9d865ff3de650198dc6b2705864d9
krishna0tak <takkrishna95@gmail.com>
2026-09-20 04:44:47 +0530

    mcp inspector and http server

--- HTTP_MCP/langchain.py
+++ HTTP_MCP/langchain.py
@@ -3,20 +3,23 @@ import asyncio
 import os
 
 #path
-mcp_server_script = os.path.join((os.path.dirname(os.path.abspath(__file__))),"first_mcp_server_stdio.py")
+mcp_server_script = os.path.join((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),"createmcp","first_mcp_server_stdio.py") 
 venv_path = os.path.join((os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),".venv")
 async def main():
     #instance of the MUltiservermcpclient
     client = MultiServerMCPClient(
     #MCP Server config
     {
-       "data_fetch_mcp_stdio": {
-    "transport": "stdio",
-    "command": os.path.join(venv_path, "Scripts", "python.exe"),
-    "args": [
-        mcp_server_script
-    ],
-}
+        "data_fetch_mcp_stdio":{
+            "transport": "stdio",
+            "command": os.path.join(venv_path, "Scripts", "python.exe"),
+            "args": [str(mcp_server_script)]
+        },
+        "data_fetch_mcp_http":{
+            "transport": "streamable-http",
+            "url": "http://localhost:8050/mcp"
+        }
+
     }
     )
     #list tools
